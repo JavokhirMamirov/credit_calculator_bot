191b3e2e8dbf3f88bac3cb7c9111d9c299e6f8ff
Mamirov1997 <mamirovjavoxir@gmail.com>
2021-12-16 09:48:00 +0500

    bot done

--- annuitet_calc.py
+++ annuitet_calc.py
@@ -1,25 +1,51 @@
+
+# def annuitet_calculator(amount, percent, months):
+#     i = percent / (100 * 12)
+#     monthly_payment = round(((i * pow((1 + i), months)) / (pow((1 + i), months) - 1)) * amount, 2)
+#     s1 = round(amount * i, 2)
+#     payed_amount = round(monthly_payment - s1, 2)
+#     total_percent = s1
+#     for j in range(2, months + 1):
+#         payed_value = round((amount - payed_amount) * i, 2)
+#         py_amount = round(monthly_payment - payed_value, 2)
+#         payed_amount += py_amount
+#         total_percent += payed_value
+#
+#
+#     Text = "KREDIT MA'LUMOTLARI\n" \
+#            f"Kredit miqdori : {amount} so'm\n" \
+#            f"Muddati : {months} oy\n" \
+#            f"Yillik foiz stavkasi: {percent}%\n" \
+#            f"Jami foiz : {round(total_percent,2)} so'm\n" \
+#            f"Jami to'lov miqdori: {amount+total_percent} so'm"
+#
+#     return Text
+
+
 def annuitet_calculator(amount, percent, months):
     i = percent / (100 * 12)
     monthly_payment = round(((i * pow((1 + i), months)) / (pow((1 + i), months) - 1)) * amount, 2)
     s1 = round(amount * i, 2)
-    payed_amount = round(monthly_payment - s1,2)
+    payed_amount = round(monthly_payment - s1, 2)
     total_percent = s1
-    Text = "KREDIT MA'LUMOTLARI\n" \
-           f"Kredit miqdori : {amount} so'm" \
-           f"Muddati : {months} oy\n" \
-           f"Yillik foiz stavkasi: {percent}%\n" \
-           "HISOBLASH JADVALI\n" \
-           f"1-oy\nKredit balansi:{amount} so'm\tAsosiy qarz: {payed_amount} so'm\n Foiz : {s1} so'm\tOylik to'lov: {monthly_payment} so'm\n"
+    text_monthly = "HISOBLASH JADVALI\n" \
+                   f"1-oy\nKredit balansi: {amount:,} so'm\nAsosiy qarz: {payed_amount:,} so'm\n" \
+                   f"Foiz : {s1:,} so'm\nOylik to'lov: {monthly_payment:,} so'm\n"
     for j in range(2, months + 1):
         payed_value = round((amount - payed_amount) * i, 2)
         py_amount = round(monthly_payment - payed_value, 2)
+        text_monthly += f"{j}-oy\nKredit balansi: {round(amount - payed_amount,2):,} so'm\nAsosiy qarz: {py_amount:,} so'm\n" \
+                        f"Foiz : {payed_value:,} so'm\nOylik to'lov: {monthly_payment:,} so'm\n"
+        payed_amount += py_amount
         total_percent += payed_value
-        Text += f"{j}-oy\nKredit balansi:{round(amount - payed_amount,2)} so'm\tAsosiy qarz: {py_amount} so'm\n" \
-                f"Foiz : {payed_value} so'm\tOylik to'lov: {monthly_payment} so'm\n"
-        payed_amount += round(py_amount, 2)
-    Text += f"Kredit miqdori : {amount}\tJami Foiz : {round(total_percent,2)}\t Jami to'lov : {amount + total_percent}"
-
-    return Text
 
 
-print(annuitet_calculator(20000000, 12, 24))
+    Text = "\nKREDIT MA'LUMOTLARI\n" \
+           f"KREDIT MIQDORI : {amount:,} so'm\n" \
+           f"MUDDATI : {months} oy\n" \
+           f"YILLIK FOIZ STAVKASI : {percent}%\n" \
+           f"JAMI FOIZ : {round(total_percent,2):,} so'm\n" \
+           f"JAMI TO'LOV MIQDORI : {amount+total_percent:,} so'm\n"
+    return text_monthly+Text
+
+print(annuitet_calculator(10000000, 12, 24))
--- bot.py
+++ bot.py
@@ -10,27 +10,32 @@ cur = con.cursor()
 cur.execute('''CREATE TABLE  IF NOT EXISTS users 
                (id int(14),amount int(14), months int(14), perc float(3), step int(2))''')
 
+
 def annuitet_calculator(amount, percent, months):
     i = percent / (100 * 12)
     monthly_payment = round(((i * pow((1 + i), months)) / (pow((1 + i), months) - 1)) * amount, 2)
     s1 = round(amount * i, 2)
     payed_amount = round(monthly_payment - s1, 2)
     total_percent = s1
+    text_monthly = "HISOBLASH JADVALI\n" \
+                   f"1-oy\nKredit balansi: {amount:,} so'm\nAsosiy qarz: {payed_amount:,} so'm\n" \
+                   f"Foiz : {s1:,} so'm\nOylik to'lov: {monthly_payment:,} so'm\n"
     for j in range(2, months + 1):
         payed_value = round((amount - payed_amount) * i, 2)
         py_amount = round(monthly_payment - payed_value, 2)
+        text_monthly += f"{j}-oy\nKredit balansi: {round(amount - payed_amount,2):,} so'm\nAsosiy qarz: {py_amount:,} so'm\n" \
+                        f"Foiz : {payed_value:,} so'm\nOylik to'lov: {monthly_payment:,} so'm\n"
         payed_amount += py_amount
         total_percent += payed_value
 
 
-    Text = "KREDIT MA'LUMOTLARI\n" \
-           f"Kredit miqdori : {amount} so'm\n" \
-           f"Muddati : {months} oy\n" \
-           f"Yillik foiz stavkasi: {percent}%\n" \
-           f"Jami foiz : {round(total_percent,2)} so'm\n" \
-           f"Jami to'lov miqdori: {amount+total_percent} so'm"
-
-    return Text
+    Text = "\nKREDIT MA'LUMOTLARI\n" \
+           f"KREDIT MIQDORI : {amount:,} so'm\n" \
+           f"MUDDATI : {months} oy\n" \
+           f"YILLIK FOIZ STAVKASI : {percent}%\n" \
+           f"JAMI FOIZ : {round(total_percent,2):,} so'm\n" \
+           f"JAMI TO'LOV MIQDORI : {amount+total_percent:,} so'm\n"
+    return text_monthly+Text
 
 
 @bot.message_handler(commands=['start'])
